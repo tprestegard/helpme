062472a9c790e5c45149537ca07325bf78cc435f
Tanner Prestegard <tprestegard@gmail.com>
2020-04-01 20:13:54 -0500

    Add some placeholder commands

---
+++ helpme/commands/openssl.py
@@ -0,0 +1,38 @@
+import textwrap
+
+from ..utils import CommandGroup, SubCommand
+
+
+###############################################################################
+# Main command ################################################################
+###############################################################################
+openssl = CommandGroup(name="openssl", help="Help with openssl commands")
+BASE_COMMAND = openssl
+
+
+###############################################################################
+# Base class for subcommands ##################################################
+###############################################################################
+class OpensslSubCommand(SubCommand):
+    def __init__(self, *args, **kwargs):
+        super().__init__(openssl, *args, **kwargs)
+
+
+###############################################################################
+# Subcommands #################################################################
+###############################################################################
+# Generate encrypted file using password
+OpensslSubCommand(
+    "openssl enc -aes-256-cbc -iter 1000 -in stuff.tar.gz -out "
+    "stuff.enc.tar.gz",
+    name="encrypt-file-with-password",
+    help="Encrypt file with password",
+)
+
+# Decrypt file with password
+OpensslSubCommand(
+    "openssl enc -d -aes-256-cbc -iter 1000 -in stuff.enc.tar.gz -out "
+    "stuff.tar.gz",
+    name="decrypt-file-with-password",
+    help="Decrypt file with password",
+)
---
+++ helpme/commands/tmux.py
@@ -0,0 +1,36 @@
+import textwrap
+
+from ..utils import CommandGroup, SubCommand
+
+
+###############################################################################
+# Main command ################################################################
+###############################################################################
+tmux = CommandGroup(name="tmux", help="Help with tmux commands")
+BASE_COMMAND = tmux
+
+
+###############################################################################
+# Base class for subcommands ##################################################
+###############################################################################
+class TmuxSubCommand(SubCommand):
+    def __init__(self, *args, **kwargs):
+        super().__init__(tmux, *args, **kwargs)
+
+
+###############################################################################
+# Subcommands #################################################################
+###############################################################################
+# Share a tmux session
+TmuxSubCommand(
+    "tmux -S /tmp/shareds new -s shared",
+    name="create-shared-session",
+    help="Start a shared session",
+)
+
+# Kill a tmux pane
+TmuxSubCommand(
+    "Ctrl+b x",
+    name="kill-pane",
+    help="Kill the current pane",
+)
