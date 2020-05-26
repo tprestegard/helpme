775a43744b7cce9944db1ab3e636c0b2be09cc47
Tanner Prestegard <tprestegard@gmail.com>
2020-05-26 11:38:18 -0500

    Add kubectl commands

--- helpme/commands/kubectl.py
+++ helpme/commands/kubectl.py
@@ -21,3 +21,32 @@ class KubectlSubCommand(SubCommand):
 ###############################################################################
 # Subcommands #################################################################
 ###############################################################################
+# See all pods
+KubectlSubCommand(
+    "kubectl get pods",
+    name="view-all-pods",
+    help="View all pods",
+)
+
+# Enter a running container
+KubectlSubCommand(
+    "kubectl <container-name> -- /bin/bash -l",
+    name="enter-container",
+    help="Enter a running container",
+)
+
+# Follow logs
+KubectlSubCommand(
+    "kubectl logs -f <container-name>",
+    name="follow-logs",
+    help="Continuously output logs",
+)
+
+# Restart a container
+KubectlSubCommand(
+    'kubectl patch deployment -n <namespace> <deployment> -p "{\\"spec\\":'
+    '{\\"template\\":{\\"metadata\\":{\\"labels\\":{\\"date\\":'
+    '\\"`date +\'%s\'`\\"}}}}}',
+    name="restart-container",
+    help="Restart a running container",
+)
